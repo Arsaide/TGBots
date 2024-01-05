57f1fc0239297cd21735dc01e6ded94dab984bd7
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-05 17:14:32 +0200

    Work with DB SQLite

--- main.py
+++ main.py
@@ -1,47 +1,62 @@
 import telebot
-from telebot import types
+import sqlite3
 
 bot = telebot.TeleBot('6769893511:AAFkpVxbHZiS7Lk455FKvB0cqYKkMwSa_6M')
-
+name = None
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('Перейти на сайт')
-    btn2 = types.KeyboardButton('Удалить фото')
-    btn3 = types.KeyboardButton('Изменить текст')
-    markup.row(btn1)
-    markup.row(btn2, btn3)
-    file = open('./img.jpg', 'rb')
-    bot.send_photo(message.chat.id, file, reply_markup=markup)
-    # bot.send_message(message.chat.id, 'Hello', reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
+    conn = sqlite3.connect('arsaide.sql')
+    cur = conn.cursor()
+
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), pass varchar(50))')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегестрируем! Введи имя')
+    bot.register_next_step_handler(message, user_name)
 
 
-def on_click(message):
-    if message.text.lower() == 'перейти на сайт':
-        bot.send_message(message.chat.id, "Website is open")
-    elif message.text == 'Удалить фото':
-        bot.send_message(message.chat.id, 'Deleted')
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, 'Введите пароль')
+    bot.register_next_step_handler(message, user_pass)
 
 
-@bot.message_handler(content_types=['photo'])
-def getPhoto(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://github.com/Arsaide')
-    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
-    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
-    markup.row(btn1)
-    markup.row(btn2, btn3)
-    bot.reply_to(message, 'Какое красиво фото!', reply_markup=markup)
+def user_pass(message):
+    password = message.text.strip()
+    conn = sqlite3.connect('arsaide.sql')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users(name, pass) VALUES ('%s', '%s')" % (name, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton('Список пользователей', callback_data='users'))
+    bot.send_message(message.chat.id, 'Введите пароль', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: True)
-def callback_message(callback):
-    if callback.data == 'delete':
-        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
-    elif callback.data == 'edit':
-        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    conn = sqlite3.connect('arsaide.sql')
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+    info = ''
+    for el in users:
+        info += f'Name: {el[1]}, pass: {el[2]}\n'
+
+    cur.close()
+    conn.close()
+
+    bot.send_message(call.message.chat.id, info)
 
 
 bot.polling(none_stop=True)
