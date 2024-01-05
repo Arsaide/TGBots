071854ec6ea69be15797326b58019272cbc7ac1c
Kirill <93765186+Arsaide@users.noreply.github.com>
2024-01-05 16:38:36 +0200

    work

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import telebot
-import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('6769893511:AAFkpVxbHZiS7Lk455FKvB0cqYKkMwSa_6M')
 
-@bot.message_handler(commands=['site', 'website'])
-def site(message):
-    webbrowser.open('https://github.com/Arsaide')
 
 @bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('Перейти на сайт')
+    btn2 = types.KeyboardButton('Удалить фото')
+    btn3 = types.KeyboardButton('Изменить текст')
+    markup.row(btn1)
+    markup.row(btn2, btn3)
+    file = open('./img.jpg', 'rb')
+    bot.send_photo(message.chat.id, file, reply_markup=markup)
+    # bot.send_message(message.chat.id, 'Hello', reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
 
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == 'hello':
-        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
-    elif message.text.lower() == 'id':
-        bot.reply_to(message, f'ID: {message.from_user.id}')
+
+def on_click(message):
+    if message.text.lower() == 'перейти на сайт':
+        bot.send_message(message.chat.id, "Website is open")
+    elif message.text == 'Удалить фото':
+        bot.send_message(message.chat.id, 'Deleted')
+
+
+@bot.message_handler(content_types=['photo'])
+def getPhoto(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://github.com/Arsaide')
+    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
+    markup.row(btn1)
+    markup.row(btn2, btn3)
+    bot.reply_to(message, 'Какое красиво фото!', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'delete':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+    elif callback.data == 'edit':
+        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
 
 
 bot.polling(none_stop=True)
